ae54f71aeb1fbf7d9f3fd15483bf36090b176ca0
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2020-09-06 20:05:09 +0530

    UI Changes
    SPLASHSCREEN ,LOGIN PAGE &  SIGN UP PAGE UI UPDATED according to Adobe XD

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -4,116 +4,122 @@ import 'package:library_system/pages/signup.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
-  static String id= 'login_page';
+  static String id = 'login_page';
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final _auth=FirebaseAuth.instance;
+  final _auth = FirebaseAuth.instance;
   String email;
   String password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFFFFFF),
       body: SafeArea(
         child: Padding(
-        padding: EdgeInsets.only(left:8.0,right: 8.0),
+          padding: EdgeInsets.only(left: 8.0, right: 8.0),
           child: ListView(
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-            //    SizedBox(height: 80),
-                Image(image: NetworkImage(
-                  //  'https://www.netclipart.com/pp/m/79-791996_icons-and-graphics-stack-of-books-transparent.png'
-                'https://i.dlpng.com/static/png/4262526-cartoon-teachers-day-calendar-is-a-commercial-element-calendar-teacher-calendar-png-640_640_preview.webp'
-                ),
-                ),
-                SizedBox(height:16),
-                Text('Library Management System',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),),
-              ],
-            ),
-            SizedBox(height:60),
-            Padding(
-              padding: const EdgeInsets.only(left: 10,right: 10),
-              child: TextField(
-                onChanged: (value){
-                  email=value;
-                },
-                decoration: InputDecoration(
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.brown),
-                  ),
-                  hintText: '\tEmail',
-                  icon: Icon(Icons.email,color: Colors.brown),
-                ),
-              ),
-            ),
-            SizedBox(height: 14),
-            Padding(
-              padding: const EdgeInsets.only(left:10,right:10),
-              child: TextField(
-                onChanged: (value){
-                  password=value;
-                },
-                decoration: InputDecoration(
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.brown),
-                  ),
-                  hintText: 'Password',
-                  icon: Icon(Icons.lock, color: Colors.brown),
-                ),
-                obscureText: true,//replaces password with bullets as we enter it
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top:5.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
                 children: <Widget>[
-                  RaisedButton(
-                    padding: EdgeInsets.only(left: 50,right: 50),
-                    shape: StadiumBorder(),
-                    color: Color(0xFF8AC7C0),
-                    onPressed: () async{
-                      try{
-                        final user= await _auth.signInWithEmailAndPassword(email: email, password: password);
-                        if(user!=null)
-                          Navigator.pushNamed(context, HomePage.id);
-                      }catch(e){
-                        print(e);
-                      }
-                    },
-                    child: Text('LOGIN',
-                    ),
-                  )
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Image.asset('assets/images/library.jpg'),
                 ],
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 30, 10, 10),
-              child: Column(
-                children: <Widget>[
-                  Text('Don\'t have an account?'),
-                  RaisedButton(
-                    shape: StadiumBorder(),
-                    color: Color(0xFF8AC7C0),
-                    onPressed: (){
-                      Navigator.pushNamed(context, SignUpPage.id);
-                      print('push');
-                    },
-                    child: Text('SIGN UP',
+              SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: TextField(
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Color(0xFF584846),
                     ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.brown),
+                    ),
+                    hintText: '\tEnter Email-id',
                   ),
-                ],
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 14),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: TextField(
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Color(0xFF584846),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF584846)),
+                    ),
+                    hintText: 'Enter Password',
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 30,
+                    ),
+                    RaisedButton(
+                      padding: EdgeInsets.only(left: 40, right: 40),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14.0),
+                      ),
+                      color: Color(0xFF584846),
+                      onPressed: () async {
+                        try {
+                          final user = await _auth.signInWithEmailAndPassword(
+                              email: email, password: password);
+                          if (user != null)
+                            Navigator.pushNamed(context, HomePage.id);
+                        } catch (e) {
+                          print(e);
+                        }
+                      },
+                      child:
+                          Text('LOGIN', style: TextStyle(color: Colors.white)),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    RaisedButton(
+                      padding: EdgeInsets.only(left: 40, right: 40),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14.0),
+                      ),
+                      color: Color(0xFF584846),
+                      onPressed: () {
+                        Navigator.pushNamed(context, SignUpPage.id);
+                        print('push');
+                      },
+                      child: Text(
+                        'SIGN UP',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
       ),
     );
   }
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -3,101 +3,107 @@ import 'package:library_system/pages/home.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class SignUpPage extends StatefulWidget {
-  static String id='signup_page';
+  static String id = 'signup_page';
   @override
   _SignUpPageState createState() => _SignUpPageState();
 }
 
 class _SignUpPageState extends State<SignUpPage> {
-  final _auth=FirebaseAuth.instance;
+  final _auth = FirebaseAuth.instance;
   String email;
   String password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child:
-      Padding(
-        padding: EdgeInsets.all(8.0),
-        child: ListView(
-          children: <Widget>[
-          //  SizedBox(height: 80),
-            Column(
-              children: <Widget>[
-                Image(image: NetworkImage(
-                    //'https://www.netclipart.com/pp/m/79-791996_icons-and-graphics-stack-of-books-transparent.png'
-                    'https://i.dlpng.com/static/png/4262526-cartoon-teachers-day-calendar-is-a-commercial-element-calendar-teacher-calendar-png-640_640_preview.webp'
-                  ),
-                ),
-                SizedBox(height:16),
-                Text('Library Management System',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),),
-              ],
-            ),
-            SizedBox(height:60),
-            Padding(
-              padding: const EdgeInsets.only(left: 10,right: 10),
-              child: TextField(
-                onChanged: (value){
-                  email=value;
-                },
-                // controller: _usernameController,
-                decoration: InputDecoration(
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.brown),
-                  ),
-                  hintText: '\tEmail',
-                  icon: Icon(Icons.email, color: Colors.brown,),
-                ),
-              ),
-            ),
-            SizedBox(height: 14),
-            Padding(
-              padding: const EdgeInsets.only(left:10,right:10),
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value){
-                  password=value;
-                },
-                //  controller: _passwordController,
-                decoration: InputDecoration(
-                  hoverColor: Colors.brown,
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.brown),
-                  ),
-                  hintText: 'Password',
-                  icon: Icon(Icons.lock, color: Colors.brown),
-                ),
-                obscureText: true,//replaces password with bullets as we enter it
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 30, 10, 10),
-              child: Column(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: ListView(
+            children: <Widget>[
+              Column(
                 children: <Widget>[
-                  Text('Continue for a seamless experience!'),
-                  RaisedButton(
-                    padding: EdgeInsets.only(left: 40,right: 40),
-                    shape: StadiumBorder(),
-                    color: Color(0xFF8AC7C0),
-                     onPressed: () async{
-                      try{
-                        final newuser= await _auth.createUserWithEmailAndPassword(email: email, password: password);
-                        if(newuser!=null)
-                          Navigator.pushReplacementNamed(context, HomePage.id);
-                      }catch(e){
-                        print(e);
-                      }
-                    },
-                    child: Text('SIGN UP'),
+                  SizedBox(
+                    height: 15,
                   ),
+                  Image.asset('assets/images/library.jpg'),
                 ],
               ),
-            ),
-          ],
+              SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: TextField(
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  // controller: _usernameController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Color(0xFF584846),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF584846)),
+                    ),
+                    hintText: '\tEnter Email-id',
+                  ),
+                ),
+              ),
+              SizedBox(height: 14),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  //  controller: _passwordController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Color(0xFF584846),
+                    ),
+                    hoverColor: Colors.brown,
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.brown),
+                    ),
+                    hintText: 'Enter Password',
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                child: Column(
+                  children: <Widget>[
+                    RaisedButton(
+                      padding: EdgeInsets.only(left: 40, right: 40),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14.0),
+                      ),
+                      color: Color(0xFF584846),
+                      onPressed: () async {
+                        try {
+                          final newuser =
+                              await _auth.createUserWithEmailAndPassword(
+                                  email: email, password: password);
+                          if (newuser != null)
+                            Navigator.pushReplacementNamed(
+                                context, HomePage.id);
+                        } catch (e) {
+                          print(e);
+                        }
+                      },
+                      child: Text(
+                        'SIGN UP',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
       ),
     );
   }
