cd7896d4d3d5eacad69b2bc518023155d3596cf0
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2020-09-11 18:04:45 +0530

    splashscreeen update

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:library_system/pages/home.dart';
 import 'package:library_system/pages/login.dart';
 import 'package:library_system/pages/signup.dart';
-import 'package:splashscreen/splashscreen.dart';
+import 'package:library_system/pages/splashScreen.dart';
 
 void main() {
   runApp(Library());
@@ -13,14 +13,7 @@ class Library extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(
-        seconds: 2,
-        backgroundColor: Color(0xFFF4D0AB),
-        loaderColor: Colors.white,
-        image: Image.asset('assets/images/book.png'),
-        photoSize: 150.0,
-        navigateAfterSeconds: LoginPage(),
-      ),
+      home: SplashScreen(),
       routes: {
         LoginPage.id: (context) => LoginPage(),
         SignUpPage.id: (context) => SignUpPage(),
