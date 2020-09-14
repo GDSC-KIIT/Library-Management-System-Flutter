2e8c34b8487f5a82402c750e056b3d1dd81df96e
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2020-09-14 21:22:58 +0530

    Profile page added

---
+++ lib/pages/editProfilePage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:library_system/pages/profilepage.dart';
+
+class EditProfilePage extends StatefulWidget {
+  EditProfilePage({Key key}) : super(key: key);
+
+  @override
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  @override
+  Widget build(BuildContext context) {}
+}
--- lib/pages/profilepage.dart
+++ lib/pages/profilepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:library_system/pages/editProfilePage.dart';
+import 'package:library_system/pages/homePage.dart';
 
 class ProfilePage extends StatefulWidget {
   ProfilePage({Key key}) : super(key: key);
@@ -10,13 +12,197 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Container(
-        child: Container(
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text("Profile Page"),
-            ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                height: 500,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 499,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFE0B485),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(0.0),
+                          bottomLeft: Radius.circular(27.0),
+                          bottomRight: Radius.circular(27.0),
+                          topRight: Radius.circular(0.0),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: CircleAvatar(
+                        radius: 80,
+                        backgroundColor: Color(0xFFE0B485),
+                        child: Opacity(
+                          opacity: 0.8,
+                          child: CircleAvatar(
+                            radius: 80,
+                            backgroundImage: NetworkImage(
+                                'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.bottomCenter,
+                      height: 335,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'FULL NAME',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontFamily: 'Montserrat',
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.bottomCenter,
+                      height: 390,
+                      child: Opacity(
+                        opacity: 0.7,
+                        child: Text(
+                          'Admin',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    leading: Icon(
+                      Icons.call,
+                      color: Color(0xFF584846),
+                    ),
+                    title: Text(
+                      'Phone Number',
+                      style: TextStyle(
+                          color: Color(0xFF584846),
+                          fontFamily: 'Montserrat',
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      '+ 91 0000000000',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.mail,
+                      color: Color(0xFF584846),
+                    ),
+                    title: Text(
+                      'Email',
+                      style: TextStyle(
+                          color: Color(0xFF584846),
+                          fontFamily: 'Montserrat',
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      'admin@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 30,
+                      ),
+                      Expanded(
+                        child: MaterialButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12)),
+                          color: Color(0xFF584846),
+                          child: Text("Edit",
+                              style: TextStyle(color: Colors.white)),
+                          // onPressed: () {
+                          //   Navigator.push(
+                          //     context,
+                          //     MaterialPageRoute(
+                          //         builder: (context) => EditProfilePage()),
+                          //   );
+                          // },
+                        ),
+                      ),
+                      SizedBox(
+                        width: 30,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => HomePage(),
+                            ),
+                          );
+                        },
+                        child: Icon(
+                          Icons.home,
+                          color: Color(0xFF584846),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 30,
+                      ),
+                      Expanded(
+                        child: MaterialButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12)),
+                          color: Color(0xFF584846),
+                          child: Text("Logout",
+                              style: TextStyle(color: Colors.white)),
+                          onPressed: () {},
+                        ),
+                      ),
+                      SizedBox(
+                        width: 30,
+                      ),
+                    ],
+                  ),
+                  // Row(
+                  //   children: [
+                  //     Container(
+                  //       child: MaterialButton(
+                  //         shape: RoundedRectangleBorder(
+                  //             borderRadius: BorderRadius.circular(12)),
+                  //         color: Colors.red,
+                  //         child: Text('Logout'),
+                  //         onPressed: () {},
+                  //       ),
+                  //     ),
+                  //   ],
+                  // )
+                ],
+              )
+            ],
           ),
         ),
       ),
